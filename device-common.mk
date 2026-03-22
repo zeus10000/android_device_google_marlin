49a81513921045b93b66dc17b515f15d6223e3d7
Builder <builder@localhost>
2026-03-22 06:26:43 +0000

    marlin: add libhubconnection and libdynamic_sensor_ext to PRODUCT_PACKAGES
    sensors.nanohub.so depends on both libhubconnection.so and libdynamic_sensor_ext.so
    at runtime. Without these, the HIDL sensor service fails to load with:
      libhubconnection.so not found
    
    libhubconnection: built from device/google/contexthub/sensorhal/ (new Android.bp)
    libdynamic_sensor_ext: built from hardware/libhardware/modules/sensors/dynamic_sensor/
    
    Change-Id: Imarlin4sensor4deps4libhubconnection

--- device-common.mk
+++ device-common.mk
@@ -376,6 +376,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
     sensors.nanohub \
+    libhubconnection \
+    libdynamic_sensor_ext \
     nanoapp_cmd
 
 PRODUCT_COPY_FILES += \
